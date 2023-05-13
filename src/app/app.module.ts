54b293ade4ff5f184a8f4a9eb118d8bbd957539e
Lules03 <lescanolucia27@gmail.com>
2023-05-13 20:08:46 -0300

    frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
@@ -13,6 +12,11 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -26,12 +30,16 @@ import { FooterComponent } from './components/footer/footer.component';
     HysComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
   
   ],
   imports: [
     BrowserModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    RouterModule,
 
   ],
   providers: [],
